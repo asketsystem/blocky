e15ffec2ca129573238f56eb975efa6ae75973fd
asketsystem <asketsystem1@gmail.com>
2022-04-17 08:43:37 +0000

    writing the first genesis block

---
+++ chain.js
@@ -0,0 +1,42 @@
+const Block = require("./block").Block;
+const BlockHeader = require("./block").BlockHeader;
+const moment = require("moment");
+
+let getGenesisBlock = () => {
+  let blockHeader = new BlockHeader(
+    1,
+    null,
+    "0x1bc1100000000000000000000000000000000000000000000",
+    moment().unix(),
+    "0x171b7320",
+    "1CAD2B8C"
+  );
+  return new Block(blockHeader, 0, null);
+};
+
+let getLatestBlock = () => blockchain[blockchain.length - 1];
+
+let addBlock = (newBlock) => {
+  let prevBlock = getLatestBlock();
+  if (
+    prevBlock.index < newBlock.index &&
+    newBlock.blockHeader.previousBlockHeader ===
+      prevBlock.blockHeader.merkleRoot
+  ) {
+    blockchain.push(newBlock);
+  }
+};
+
+let getBlock = (index) => {
+  if (blockchain.length - 1 >= index) return blockchain[index];
+  else return null;
+};
+
+const blockchain = [getGenesisBlock()];
+
+if (typeof exports != "undefined") {
+  exports.addBlock = addBlock;
+  exports.getBlock = getBlock;
+  exports.getLatestBlock = getLatestBlock;
+  exports.blockchain = blockchain;
+}
