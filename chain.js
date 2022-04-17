11fc47972b1b5dcb12c94289b73f67120733fe12
asketsystem <asketsystem1@gmail.com>
2022-04-17 23:39:23 +0000

    installing LevelDB

--- chain.js
+++ chain.js
@@ -1,6 +1,19 @@
-const Block = require("./block").Block;
-const BlockHeader = require("./block").BlockHeader;
+const Block = require("./block.js").Block;
+const BlockHeader = require("./block.js").BlockHeader;
 const moment = require("moment");
+const CryptoJS = require("crypto-js");
+const { Level } = require("level");
+const fs = require("fs");
+let db;
+
+let createDb = (peerId) => {
+  let dir = __dirname + "/db/" + peerId;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    db = new Level(dir);
+    storeBlock(getGenesisBlock());
+  }
+};
 
 let getGenesisBlock = () => {
   let blockHeader = new BlockHeader(
@@ -24,7 +37,23 @@ let addBlock = (newBlock) => {
       prevBlock.blockHeader.merkleRoot
   ) {
     blockchain.push(newBlock);
+    storeBlock(newBlock); // When you generate a new block using the generateNextBlock method, you can now store the block in the LevelDB database
   }
+};
+
+// create a storeBlock method to store the new block
+let storeBlock = (newBlock) => {
+  db.put(newBlock.index, JSON.stringify(newBlock), function (err) {
+    if (err) return console.log("Ooops!", err); // some kind of I/O error
+    console.log("--- Inserting block index: " + newBlock.index);
+  });
+};
+
+let getDbBlock = (index, res) => {
+  db.get(index, function (err, value) {
+    if (err) return res.send(JSON.stringify(err));
+    return res.send(value);
+  });
 };
 
 let getBlock = (index) => {
@@ -34,9 +63,31 @@ let getBlock = (index) => {
 
 const blockchain = [getGenesisBlock()];
 
+const generateNextBlock = (txns) => {
+  const prevBlock = getLatestBlock(),
+    prevMerkleRoot = prevBlock.blockHeader.merkleRoot;
+  (nextIndex = prevBlock.index + 1),
+    (nextTime = moment().unix()),
+    (nextMerkleRoot = CryptoJS.SHA256(1, prevMerkleRoot, nextTime).toString());
+
+  const blockHeader = new BlockHeader(
+    1,
+    prevMerkleRoot,
+    nextMerkleRoot,
+    nextTime
+  );
+  const newBlock = new Block(blockHeader, nextIndex, txns);
+  blockchain.push(newBlock);
+  storeBlock(newBlock);
+  return newBlock;
+};
+
 if (typeof exports != "undefined") {
   exports.addBlock = addBlock;
   exports.getBlock = getBlock;
-  exports.getLatestBlock = getLatestBlock;
   exports.blockchain = blockchain;
+  exports.getLatestBlock = getLatestBlock;
+  exports.generateNextBlock = generateNextBlock;
+  exports.createDb = createDb;
+  exports.getDbBlock = getDbBlock;
 }
